527f9e70c7d259e48f33fefafa8f8ce28db345a8
shyam-darkhorse <30753805+shyam-darkhorse@users.noreply.github.com>
2017-08-07 20:12:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/article-one.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two.html', function (req, res) {
-  res.send('Article-two requested');
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three.html', function (req, res) {
-  res.send('Article-three requested');
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
